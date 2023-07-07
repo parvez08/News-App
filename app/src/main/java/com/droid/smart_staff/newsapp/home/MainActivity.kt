17483ea789803b5323f0644a034d70444b0d0cc5
parvez08 <parvezshaikh.0894@gmail.com>
2023-07-07 18:08:04 +0530

    - Setting up binding for main activity.

---
+++ app/src/main/java/com/droid/smart_staff/newsapp/home/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.droid.smart_staff.newsapp.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.newsapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
