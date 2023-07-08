b3f0b3f942e68b13ca61b24e5bf1b6a2c10a736f
Parvez Shaikh 2 <parvezshaikh.0894@gmail.com>
2023-07-08 11:44:06 +0530

    - Created SplashScreenActivity.
    - Added lottie animation file.

--- app/src/main/java/com/droid/smart_staff/newsapp/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/droid/smart_staff/newsapp/splash/SplashScreenActivity.kt
@@ -1,13 +1,15 @@
 package com.droid.smart_staff.newsapp.splash
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import com.droid.smart_staff.newsapp.home.MainActivity
+import com.droid.smart_staff.newsapp.home.HomeActivity
 import com.example.newsapp.databinding.ActivitySplashScreenBinding
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashScreenBinding
     private val handler: Handler = Handler(Looper.getMainLooper())
@@ -21,7 +23,7 @@ class SplashScreenActivity : AppCompatActivity() {
 
     private fun goToHomeScreen() {
         handler.postDelayed({
-            val homeIntent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            val homeIntent = Intent(this@SplashScreenActivity, HomeActivity::class.java)
             startActivity(homeIntent)
             finish()
         }, 3000)
