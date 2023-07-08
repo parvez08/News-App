656c674f7b4786d31ef25d4ab60c52d683a0fd76
Parvez Shaikh 2 <parvezshaikh.0894@gmail.com>
2023-07-08 11:41:32 +0530

    - Init view model.
    - Init recyclerview adapter & setting news adapter to rv.
    
    - Setting observer to Live data.

--- app/src/main/java/com/droid/smart_staff/newsapp/home/fragments/NewsFragment.kt
+++ app/src/main/java/com/droid/smart_staff/newsapp/home/fragments/NewsFragment.kt
@@ -1,14 +1,25 @@
 package com.droid.smart_staff.newsapp.home.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.droid.smart_staff.newsapp.news.adapters.NewsAdapter
+import com.droid.smart_staff.newsapp.news.adapters.OnNewsSelectedListenerCallback
+import com.droid.smart_staff.newsapp.news.models.NewsDataItem
+import com.droid.smart_staff.newsapp.news.utils.NewsViewModel
 import com.example.newsapp.databinding.FragmentNewsBinding
 
 class NewsFragment : Fragment() {
     private lateinit var binding: FragmentNewsBinding
+    private var newsList: ArrayList<NewsDataItem> = arrayListOf()
+    private lateinit var mNewsAdapter: NewsAdapter
+    private val newsViewModel: NewsViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +35,54 @@ class NewsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        newsLiveDataObserver()
+        bookmarkLiveDataObserver()
+        binding.pullToRefresh.isRefreshing = true
+        newsViewModel.fetchNews()
+        setUpRecyclerView()
+        binding.pullToRefresh.setOnRefreshListener {
+            binding.pullToRefresh.isRefreshing = true
+            newsViewModel.fetchNews()
+        }
+    }
 
+    @SuppressLint("NotifyDataSetChanged")
+    private fun bookmarkLiveDataObserver() {
+        newsViewModel.getBookmarkStatusLiveData().observe(viewLifecycleOwner) {
+            binding.pullToRefresh.isRefreshing = false
+            if (!it.success) return@observe
+            mNewsAdapter.notifyDataSetChanged()
+            Toast.makeText(requireContext(), it.data?.data?.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun newsLiveDataObserver() {
+        newsViewModel.getNewsLiveData().observe(viewLifecycleOwner) {
+            binding.pullToRefresh.isRefreshing = false
+            if (!it.success) return@observe
+            if (it.data?.data == null) return@observe
+            newsList = it.data.data as ArrayList<NewsDataItem>
+            mNewsAdapter.setNewsItems(newsList)
+        }
+    }
+
+    private fun setUpRecyclerView() {
+        mNewsAdapter = NewsAdapter(
+            newsList,
+            requireContext(),
+            object : OnNewsSelectedListenerCallback {
+                @SuppressLint("NotifyDataSetChanged")
+                override fun onSelected(position: Int) {
+                    binding.pullToRefresh.isRefreshing = true
+                    newsViewModel.fetchBookmarkStatus(position, "news")
+                }
+
+            })
+        binding.rvNews.apply {
+            adapter = mNewsAdapter
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        }
     }
 
     companion object {
